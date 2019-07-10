b3dc0a4c841e2a94a37d8581e7b0e1786efe95d2
Cheng Chang <tonychangcheng@gmail.com>
2019-07-10 19:38:33 +1000

    add moving left function

--- robot.js
+++ robot.js
@@ -12,3 +12,17 @@ function onCommandRight() {
     // todo check the available of the target position
     mapCells[currentRobotLocation + 1].innerHTML = 'R';
 }
+
+function onCommandLeft() {
+    var mapCells = document.querySelectorAll('.map-cell');
+    var currentRobotLocation = 0;
+    for (var i = 0; i < mapCells.length; i++) {
+        if (mapCells[i].innerHTML.length > 0) {
+            currentRobotLocation = i;
+            break;
+        }
+    }
+    mapCells[currentRobotLocation].innerHTML = '';
+    // todo check the available of the target position
+    mapCells[currentRobotLocation - 1].innerHTML = 'R';
+}
